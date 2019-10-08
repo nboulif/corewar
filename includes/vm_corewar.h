117201850224215555a664f83609c6360e9816bb
rhunders <rayan.hunders@hotmail.com>
2019-10-08 20:34:14 +0200

    merging

--- includes/vm_corewar.h
+++ includes/vm_corewar.h
@@ -58,4 +58,22 @@ typedef struct s_op
 	int				dir_size;
 }				t_op;
 
+typedef struct		s_champs
+{
+	char			*name;
+	char			*comment;
+	void			*executable_code;
+	size_t			size_name;
+	size_t			size_comment;
+	size_t			size_executable;
+}					t_champs;
+
+typedef struct		s_all
+{
+	int				*fd;
+	t_champs		*champ;
+	size_t			nb_champ;
+    unsigned int	flag;
+}					t_all;
+
 #endif
