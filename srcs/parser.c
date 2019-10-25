e0cc22b7059de93f7ffd0bcc425f95b376d1e113
Nadja Sondag <nsondag@w-r2-p3.s19.be>
2019-10-25 16:10:06 +0200

    norme parser

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,13 +6,13 @@
 /*   By: nsondag <nsondag@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 13:06:40 by nsondag           #+#    #+#             */
-/*   Updated: 2019/10/25 14:23:16 by nsondag          ###   ########.fr       */
+/*   Updated: 2019/10/25 16:09:27 by nsondag          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-t_data		*init_data_label_only(int nb_line, char *label)
+t_data			*init_data_label_only(int nb_line, char *label)
 {
 	t_data	*data;
 
@@ -28,7 +28,7 @@ t_data		*init_data_label_only(int nb_line, char *label)
 	return (data);
 }
 
-t_data		*init_data(char *str_para, int nb_line, char *label, char *str_opc)
+t_data			*init_data(char *str_para, int nb_line, char *label, char *str_opc)
 {
 	t_data	*data;
 
@@ -65,7 +65,7 @@ t_data		*init_data(char *str_para, int nb_line, char *label, char *str_opc)
 ** sinon -> retour nb_line
 */
 
-static int	get_label(t_prog *prog)
+static int		get_label(t_prog *prog)
 {
 	t_data	*d;
 	t_data	*tmp;
@@ -94,11 +94,22 @@ static int	get_label(t_prog *prog)
 	return (0);
 }
 
-int			program_parser(t_prog *prog, t_data *data)
+static t_data	*get_pc(t_prog *prog, t_data *tmp_data, t_data *data)
+{
+	tmp_data->pc = data->pc + data->nb_octet;
+	(!prog->list_data) ? prog->list_data = tmp_data : 0;
+	data->next = tmp_data;
+	if (tmp_data->op && tmp_data->op->opc)
+		data->next->pc = data->pc + data->nb_octet;
+	data->nb_line = prog->nb_line;
+	return (data);
+}
+
+int				program_parser(t_prog *prog, t_data *data)
 {
 	t_data	*tmp_data;
 	t_data	*begin_data;
-	int		label_error_line;
+	int		label_error;
 
 	begin_data = data;
 	while (get_next_line(prog->fd, &prog->full_line) > 0)
@@ -112,28 +123,11 @@ int			program_parser(t_prog *prog, t_data *data)
 			return (manage_errors(prog, 0));
 		if (!(tmp_data = parse_commands(prog)))
 			return (1);
-		tmp_data->pc = data->pc + data->nb_octet;
-		if (tmp_data->op && tmp_data->op->opc)
-		{
-			(!prog->list_data) ? prog->list_data = tmp_data : 0;
-			data->next = tmp_data;
-			data->next->pc = data->pc + data->nb_octet;
-			data->nb_line = prog->nb_line;
-			data = data->next;
-		}
-		else if (tmp_data->label)
-		{
-			(!prog->list_data) ? prog->list_data = tmp_data : 0;
-			data->next = tmp_data;
-			data->nb_line = prog->nb_line;
-			data = data->next;
-		}
+		if ((tmp_data->op && tmp_data->op->opc) || tmp_data->label)
+			data = get_pc(prog, tmp_data, data)->next;
 	}
-	if ((label_error_line = get_label(prog)))
-	{
-		prog->i = 0;
-		return (manage_errors_inexisting_label(begin_data, label_error_line - 1));
-	}
+	if ((label_error = get_label(prog)))
+		return (manage_errors_inexisting_label(begin_data, label_error - 1));
 	prog->prog_size = data->pc + data->nb_octet;
 	close(prog->fd);
 	return (0);
