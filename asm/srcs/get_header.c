6e95c60e326dacd357db137603f033e6257dbdd2
Nadja Sondag <nsondag@e-r8-p1.s19.be>
2019-11-08 13:28:52 +0100

    wip norm header

--- asm/srcs/get_header.c
+++ asm/srcs/get_header.c
@@ -6,7 +6,7 @@
 /*   By: nsondag <nsondag@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/22 17:50:32 by nsondag           #+#    #+#             */
-/*   Updated: 2019/11/06 17:10:03 by nsondag          ###   ########.fr       */
+/*   Updated: 2019/11/08 13:28:32 by nsondag          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,13 @@
 ** line vide ou commentaire attendu apres, sinon erreur
 */
 
-char	*search_next_line(t_prog *p, int max_len)
+static char	*search_next_line(t_prog *p, int max_len)
 {
 	int		len;
-	char	*name_comment_continue;
+	char	*content_continue;
 
 	len = 0;
-	if (!((name_comment_continue) = ft_strnew(max_len)))
+	if (!((content_continue) = ft_strnew(max_len)))
 	{
 		printf("MALLOC PROBLEM\n");
 		return (NULL);
@@ -45,78 +45,104 @@ char	*search_next_line(t_prog *p, int max_len)
 		p->nb_line++;
 		p->i = 0;
 		len = skip_until(p->line, &p->i, "\"");
-		ft_strcat(name_comment_continue, "\n");
-		ft_strncat(name_comment_continue, &p->line[p->i - len], len);
+		ft_strcat(content_continue, "\n");
+		ft_strncat(content_continue, &p->line[p->i - len], len);
 		if (p->line[p->i] == '"')
 			break ;
 	}
 	if (p->line[p->i] != '"')
 		return (NULL);
-	return (name_comment_continue);
+	return (content_continue);
 }
 
-int		get_valid_name_comment(t_prog *p, int max_len, char **name_comment)
+static int	get_header_content(t_prog *p, int max_len, char **content)
 {
-	int		len_type;
 	int		len;
-	char	*name_comment_continue;
+	char	*content_continue;
 
-	len_type = max_len == PROG_NAME_LENGTH ? NAME : COMMENT;
-	p->i += len_type;
-	skip_chars(p->line, &p->i, " \t");
-	if (!(p->line[p->i]))
-		return (print_error_token(p, p->i, 0, "ENDLINE"));
-	if (p->line[p->i] != '"')
-		return (manage_errors(p, p->i));
 	p->i++;
 	len = skip_until(p->line, &p->i, "\"");
-	if (!((*name_comment) = ft_strnew(max_len)))
+	if (!((*content) = ft_strnew(max_len)))
 		return (printf("MALLOC PROBLEM\n"));
-	ft_strncpy(*name_comment, &p->line[p->i - len], len);
+	ft_strncpy(*content, &p->line[p->i - len], len);
 	if (p->line[p->i] != '"')
 	{
-		name_comment_continue = search_next_line(p, max_len);
-		if (!name_comment_continue)
+		content_continue = search_next_line(p, max_len);
+		if (!content_continue)
 			return (manage_errors(p, p->i));
 		else
 		{
-			len += ft_strlen(name_comment_continue);
-			ft_strcat(*name_comment, name_comment_continue);
+			len += ft_strlen(content_continue);
+			ft_strcat(*content, content_continue);
 		}
 	}
 	if (len > max_len)
 		return (printf("%s (Max length %d)\n", LONG_NAME, max_len));
 	p->i++;
 	skip_chars(p->line, &p->i, " \t");
-	return ((!(p->line[p->i]) || p->line[p->i] == '#') ? 0 : manage_errors(p, p->i));
+	return ((!(p->line[p->i]) || p->line[p->i] == '#') ? 0 :
+			manage_errors(p, p->i));
 }
 
-int		get_header(t_prog *p)
+static int	get_header2(t_prog *p, char **content, int type_len, int type)
 {
-	while ((p->nb_line++ >= 0) && get_next_line(p->fd, &p->line) > 0)
+	char	*error_message;
+	int		max_len;
+
+	error_message = NULL;
+	if (*content)
 	{
+		if (type == NAME_TYPE)
+			error_message = "COMMAND_NAME";
+		else if (type == COMMENT_TYPE)
+			error_message = "COMMAND_COMMENT";
+		return (print_error_token(p, 0, type_len, error_message));
+	}
+	p->i = p->i + type_len;
+	if (type == NAME_TYPE)
+		max_len = PROG_NAME_LENGTH;
+	else if (type == COMMENT_TYPE)
+		max_len = COMMENT_LENGTH;
+	skip_chars(p->line, &p->i, " \t");
+	if (!(p->line[p->i]))
+		return (print_error_token(p, p->i, 0, "ENDLINE"));
+	if (p->line[p->i] != '"')
+		return (manage_errors(p, p->i));
+	if (get_header_content(p, max_len, content))
+		return (1);
+	return (0);
+}
+
+/*
+** type_len:	length of *_CMD_STRING (".name" or ".comment")
+** ret:			return value, 0 if no error
+**
+** return:		ret
+*/
+
+int			get_header(t_prog *p)
+{
+	int		type_len;
+	int		ret;
+
+	ret = 0;
+	while (get_next_line(p->fd, &p->line) > 0)
+	{
+		type_len = 0;
+		p->nb_line++;
 		p->i = 0;
 		skip_chars(p->line, &p->i, " \t");
 		if (!p->line || !p->line[p->i] || p->line[p->i] == '#')
 			continue;
-		if (!ft_strncmp(&p->line[p->i], NAME_CMD_STRING, NAME))
-		{
-			if (p->name)
-				return (print_error_token(p, 0, NAME, "COMMAND_NAME"));
-			if (get_valid_name_comment(p, PROG_NAME_LENGTH, &p->name))
-				return (1);
-		}
-		else if (!ft_strncmp(&p->line[p->i], COMMENT_CMD_STRING, COMMENT))
-		{
-			if (p->comment)
-				return (print_error_token(p, 0, COMMENT, "COMMAND_COMMENT"));
-			if (get_valid_name_comment(p, COMMENT_LENGTH, &p->comment))
-				return (1);
-		}
+		type_len = skip_chars(&p->line[p->i + 1], &type_len, LABEL_CHARS) + 1;
+		if (!ft_strncmp(&p->line[p->i], NAME_CMD_STRING, type_len))
+			ret = get_header2(p, &p->name, type_len, NAME_TYPE);
+		else if (!ft_strncmp(&p->line[p->i], COMMENT_CMD_STRING, type_len))
+			ret = get_header2(p, &p->comment, type_len, COMMENT_TYPE);
 		else
 			return (manage_errors(p, 0));
-		if (p->name && p->comment)
+		if ((p->name && p->comment) || ret)
 			break ;
 	}
-	return (0);
+	return (ret);
 }
