e8e50e10d03ec2ca62246b81ae6f3e47d248426e
Damien Abeloos <dabeloos@w-r2-p2.s19.be>
2020-01-12 22:35:53 +0100

    champion creation

--- champion de test/champion_des_champions.s
+++ champion de test/champion_des_champions.s
@@ -2,31 +2,28 @@
 
 .comment "jtemange"
 
-#           mettre f1 en 'entier' dans les registres;
-
-record:
-    or %0,:f1,r2
-    or %0,:f1,r2
+part1:
+	ld %17367057,r3
+	ld %47186041,r2
     ld %0,r15
 
-tmp:
-    zjmp %:f1
-f1:
-#            ecrire r1 loin;        
-    st r1,511
-#            enregistrer le r1 ecrit au loin dans -> r1; aka corruption 
-    ld 511,r1
-#            ecrire remplir les registre && ecraser le code a tmp et f1 + 1
-    ld %127139840,r2
-    ld %514,r3
-    st r2,:tmp
-    sti r3,%:f1,%1
+part2:
+    st r1,128
 
-#            ecrire le code (f1) plus loin;
-    
-#            ecrire r1 en face du live a la prochaine ligne;
-    sti r1,%7,%1
-#            faire un live;
-    live %19
-#            sauter sur le code ecrit plus loin;
-    zjmp %150
+part3:
+    st r3,-8
+	st r2,-17
+
+
+
+# part4:
+# 	ld 129,r1
+# 	zjmp %17
+
+part6:
+    sti r1,%:live,%1
+live: live %19
+    #zjmp %1
+ld %0,r1
+fork %-64
+#fork %-64
