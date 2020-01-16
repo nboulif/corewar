a39d9298f3f55e458fb3b9fda96ce6734c354797
Damien Abeloos <dabeloos@w-r9-p3.s19.be>
2020-01-16 14:16:39 +0100

    champ version soumission

--- omae_wa_mou_shindeiru/omae_wa_mou_shindeiru.s
+++ omae_wa_mou_shindeiru/omae_wa_mou_shindeiru.s
@@ -2,14 +2,17 @@
 
 .comment "yolo"
 
-#sti r1,%:gengenf1,%1
 sti r1,%:genf1,%1
 ld %0,r16
 
-#gengenf1:
-#    live %0
-#    fork %:f1
-#    zjmp %:gengenf1
+# inside code
+# ld 6,r1
+# live %0
+# sti r2,r3,r16
+# sti r5,r6,r16
+# ld %0,r16
+# zjmp %63
+# 02 d0 00 06 01 01 00 00 00 00 0b 54 02 03 10 0b 54 05 06 10 02 90 00 00 00 00 10 09 00 3f
 
 genf1:
     live %0
